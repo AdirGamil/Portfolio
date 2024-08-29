6ec33e6e206bb700ed309f28a80a301d29c66177
AdirGamil <2000adir2000@gmail.com>
2024-08-29 18:45:47 +0300

    Finish header

--- src/cmps/AniHeader.jsx
+++ src/cmps/AniHeader.jsx
@@ -1,7 +1,87 @@
+import { NavLink } from 'react-router-dom'
+
 export function AniHeader() {
-    return (
-        <div>
-           hello from AniHeader
-        </div>
-    )
+  return (
+    <header className="main-header">
+      <NavLink to="/" className="header-logo">
+        <img
+          src="https://res.cloudinary.com/dhweqnxgd/image/upload/v1724940826/logo_1_i7ziuj.png"
+          alt="Adir Logo"
+          className="logo-img"
+        />
+      </NavLink>
+
+      <nav>
+        <ul className="nav-list">
+          <li className="nav-item">
+            <NavLink
+              to="/groups"
+              className={({ isActive }) =>
+                isActive ? 'active-link' : 'inactive-link'
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="/releases"
+              className={({ isActive }) =>
+                isActive ? 'active-link' : 'inactive-link'
+              }
+            >
+              About
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="/mangas"
+              className={({ isActive }) =>
+                isActive ? 'active-link' : 'inactive-link'
+              }
+            >
+              Tech Stack
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="/animes"
+              className={({ isActive }) =>
+                isActive ? 'active-link' : 'inactive-link'
+              }
+            >
+              Projects
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="/animes"
+              className={({ isActive }) =>
+                isActive ? 'active-link' : 'inactive-link'
+              }
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      
+      <div className='social-header-icon'>
+      <NavLink to="https://github.com/AdirGamil" className="header-icon">
+        <img
+          src="https://res.cloudinary.com/dhweqnxgd/image/upload/v1724941066/Vector_ywcalo.svg"
+          alt="Github icon"
+          className="github-icon"
+          />
+      </NavLink>
+      <NavLink to="https://www.linkedin.com/in/adirg/" className="header-icon">
+        <img
+          src="https://res.cloudinary.com/dhweqnxgd/image/upload/v1724941066/entypo-social_linkedin-with-circle_p9yrvm.svg"
+          alt="Linkedin"
+          className="linkein-icon"
+          />
+      </NavLink>
+          </div>
+    </header>
+  )
 }
